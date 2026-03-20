000b9e642e92a37257e312608dd4c31d1534179f
LongSNU <longdpt1002@snu.ac.kr>
2026-03-20 18:06:34 +0900

    updated contents

--- 01_Rust_Programming/01_main_println.rs
+++ 01_Rust_Programming/01_main_println.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// fn main() {}: defines a function named "main", actually this is the main function that will wrap "smaller" functions, and run (like def main() in Python)
+// {}: the function body is wrapped in this brackets {}
+// println!(): The function name is "println", the "!" is called Rust macros (explained later)
+// "Hello, world!": The string that needs to be printed out
+// The semi-colon ";": denotes that the expression println!() is over, and the next one is ready to begin
