da8b2d03e7dd69d97d3fa913de1b4d56370604bb
LongSNU <longdpt1002@snu.ac.kr>
2026-03-31 17:44:50 +0900

    updated contents

---
+++ part_01_rust_programming/src/bin/02_io_GuessingString.rs
@@ -0,0 +1,48 @@
+use std::io;
+// io stands for Input/Output, a library to obtain user input and then print the results as output
+// std stands for Standard, is the standard library (where io library and many others come from)
+
+// By default, Rust has a set of items defined in the standard library that it brings into the scope of every program. 
+// This set is called the "prelude"
+// If something you want is not in the "prelude" (like std::io), you have to bring it in
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // use "let mut" to declare a MUTABLE variable to store user input
+                                           // String::new() is a function that returns a new instance of a String
+                                           // the "::" indicates that "new()" is an association function of String
+                                           // so, this String::new() returns an empty string which the variable "guess" is bound to
+
+    io::stdin() // or can use std::io::stdin() if we had not imported std::io before
+        .read_line(&mut guess) 
+        .expect("Failed to read line");
+
+    // .read_line(&mut guess) call out read_line() from stdin() to take the user input, stored into "guess"
+    // that's why "guess" must be mutable so that the method .read_line() can change its content to take in user input
+    
+    // the "&" indicates this argument (the user input) is a reference, letting other parts of the code access this piece of data without copying this data into memory multiple times
+    // reference is also immutable by default, that's why we write "&mut guess", not "&guess"
+
+    // .read_line() returns not only the user input, but also a Result value (an enumeration)
+    // "Result" has 2 states: "Ok" (successful operation) or "Err" (failed operation)
+    // "Result" has the .expect("message") method to handle "Err" cases -> it will cause the program to crash and display the "message"
+    // If the state is "Ok", then "Result" will return the user input it is holding so that you can use it
+    // (If we not call .expet("message") out, Rust will display a warning during compiling)
+    
+    // one-liner: io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {guess}"); // The {} is a placeholder, to print out the value of the "guess" variable
+}
+
+/*
+╰─ $ cargo run -p part_01_rust_programming --bin 02_io_GuessingString
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.06s
+     Running `target/debug/02_io_GuessingString`
+Guess the number!
+Please input your guess.
+Rust is so cool!!!!
+You guessed: Rust is so cool!!!!
+*/
