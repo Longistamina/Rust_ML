e89ae994da0cec870978e8f2922e1e55b8562653
LongSNU <longdpt1002@snu.ac.kr>
2026-03-31 17:49:26 +0900

    updated contents

--- part_01_rust_programming/src/bin/02_io_InputOutput.rs
+++ part_01_rust_programming/src/bin/02_io_InputOutput.rs
@@ -7,24 +7,24 @@ use std::io;
 // If something you want is not in the "prelude" (like std::io), you have to bring it in
 
 fn main() {
-    println!("Guess the number!");
+    println!("Print the information!");
 
-    println!("Please input your guess.");
+    println!("Please input your information.");
 
-    let mut guess = String::new(); // use "let mut" to declare a MUTABLE variable to store user input
+    let mut information = String::new(); // use "let mut" to declare a MUTABLE variable to store user input
                                            // String::new() is a function that returns a new instance of a String
                                            // the "::" indicates that "new()" is an association function of String
-                                           // so, this String::new() returns an empty string which the variable "guess" is bound to
+                                           // so, this String::new() returns an empty string which the variable "information" is bound to
 
     io::stdin() // or can use std::io::stdin() if we had not imported std::io before
-        .read_line(&mut guess) 
+        .read_line(&mut information) 
         .expect("Failed to read line");
 
-    // .read_line(&mut guess) call out read_line() from stdin() to take the user input, stored into "guess"
-    // that's why "guess" must be mutable so that the method .read_line() can change its content to take in user input
+    // .read_line(&mut information) call out read_line() from stdin() to take the user input, stored into "information"
+    // that's why "information" must be mutable so that the method .read_line() can change its content to take in user input
     
     // the "&" indicates this argument (the user input) is a reference, letting other parts of the code access this piece of data without copying this data into memory multiple times
-    // reference is also immutable by default, that's why we write "&mut guess", not "&guess"
+    // reference is also immutable by default, that's why we write "&mut information", not "&information"
 
     // .read_line() returns not only the user input, but also a Result value (an enumeration)
     // "Result" has 2 states: "Ok" (successful operation) or "Err" (failed operation)
@@ -32,17 +32,18 @@ fn main() {
     // If the state is "Ok", then "Result" will return the user input it is holding so that you can use it
     // (If we not call .expet("message") out, Rust will display a warning during compiling)
     
-    // one-liner: io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // one-liner: io::stdin().read_line(&mut information).expect("Failed to read line");
 
-    println!("You guessed: {guess}"); // The {} is a placeholder, to print out the value of the "guess" variable
+    println!("Your given information: {information}"); // The {} is a placeholder, to print out the value of the "information" variable
 }
 
 /*
-╰─ $ cargo run -p part_01_rust_programming --bin 02_io_GuessingString
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.06s
-     Running `target/debug/02_io_GuessingString`
-Guess the number!
-Please input your guess.
-Rust is so cool!!!!
-You guessed: Rust is so cool!!!!
+╰─ $ cargo run -p part_01_rust_programming --bin 02_io_InputOutput
+   Compiling part_01_rust_programming v0.1.0 (/home/longdpt/Documents/Long_AISDL/Rust_ML/part_01_rust_programming)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.13s
+     Running `target/debug/02_io_InputOutput`
+Print the information!
+Please input your information.
+Rust is so cool!!!
+Your given information: Rust is so cool!!!
 */
