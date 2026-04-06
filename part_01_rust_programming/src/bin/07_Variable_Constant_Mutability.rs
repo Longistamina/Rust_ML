fb177de2d47a65c306c04a9718bfcb8631f503db
Longistamina <dophamthelong03@gmail.com>
2026-04-06 18:00:11 +0900

    updated contents

---
+++ part_01_rust_programming/src/bin/07_Variable_Constant_Mutability.rs
@@ -0,0 +1,42 @@
+fn main() {
+    // --------------------------------------------------- //
+    // ---------------- Immutable variable ----------------//
+    // --------------------------------------------------- //
+
+    let x = 5; // immutable variable
+    println!("The value of x is: {x}");
+
+    // x = 6; // this will cause a compile-time error because x is immutable
+
+
+    // ------------------------------------------------- //
+    // ---------------- Mutable variable ----------------//
+    // ------------------------------------------------- //
+
+    let mut y = 10; // mutable variable
+    println!("The value of y is: {y}");
+
+    y = 15; // this is allowed because y is mutable
+    println!("The value of y is now: {y}");
+    
+
+    // ------------------------------------------ //
+    // ---------------- Constant ----------------//
+    // ---------------------------------------- //
+
+    const MAX_POINTS: u32 = 100_000; // constant variable
+    const PI: f64 = 3.14159; // another constant variable
+    println!("The maximum points are: {MAX_POINTS}");
+    println!("The value of PI is: {PI}");
+
+    // To declare a constant, use the `const` keyword, followed by the name of the constant
+    // Constants are always immutable, so the expression assigned to a constant must be a constant expression, not a value that could be computed at runtime.
+    // Cannot use the `mut` keyword with constants, as they are inherently immutable.
+    // Constants are conventionally written in uppercase with underscores separating words.
+
+    // When declaring a constant, you must also specify its type, as constants do not have a default type. 
+    // This is because the value of a constant must be known at compile time, and the compiler needs to know the type to determine how much memory to allocate for it.
+
+    // Operations that can be used when declaring constants: https://doc.rust-lang.org/reference/const_eval.html
+
+}
---
+++ part_01_rust_programming/src/bin/08_Assigning_Shadowing.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // ------------------------------------------ //
+    // ---------------- Assigning ----------------//
+    // ------------------------------------------ //
+
+    let mut x = 5; // mutable variable
+    x = x + 1; // reassigning x to a new value
+    println!("The value of x is: {x}"); // this will print 6
+
+    // ------------------------------------------ //
+    // ---------------- Shadowing ----------------//
+    // ------------------------------------------ //
+
+    let y = 10; // immutable variable
+    let y = y + 1; // shadowing y with a new variable that has the same name
+    println!("The value of y is: {y}"); // this will print 11
+
+    // const Z: u32 = 20; // constant variable
+    // let Z = Z + 1; // shadowing Z with a new variable that has the same name
+    // println!("The value of Z is: {Z}"); // this will rais error
+}
