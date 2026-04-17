300b85bef831ccb6447c117e28dd7d8fe872f4c2
Longistamina <dophamthelong03@gmail.com>
2026-04-17 18:16:31 +0900

    updated contents

--- part_01_rust_programming/src/bin/15_loop_while_for_break_continue_pass.rs
+++ part_01_rust_programming/src/bin/15_loop_while_for_break_continue_pass.rs
@@ -13,7 +13,7 @@ fn _demo_loop_infinite() { // Use "_" before function name to avoid compiler's w
 // ------------------ break ------------------- //
 // -------------------------------------------- //
 
-fn demo_break() {
+fn _demo_break() {
     let mut counter = 0;
 
     let result = loop {
@@ -36,7 +36,7 @@ fn demo_break() {
 // Loop can be nested to handle complicated logic. However, this can affect readability
 // => Use labeled loop to enhance readibility
 
-fn demo_nested_loop_with_labels() {
+fn _demo_nested_loop_with_labels() {
     let mut count = 0;
     
     'counting_up: loop { // use single quote ' to name the outer loop with a label
@@ -76,8 +76,90 @@ fn demo_nested_loop_with_labels() {
 // Remaining = 10
 }
 
+
+// --------------------------------------------------------------------- //
+// ------------------ while loop with exit condition ------------------- //
+// --------------------------------------------------------------------- //
+
+fn _demo_while() {
+    let mut number = 5;
+
+    while number != 0 { // While the condition is true, the loop runs. 
+        println!("number = {number}");
+        number -= 1;
+    }
+
+    println!("While loop done!")
+}
+
+
+// ---------------------------------------------------------------- //
+// ------------------ for loop with collections ------------------- //
+// ---------------------------------------------------------------- //
+
+fn _demo_for() {
+    let arr = [10, 20, 30, 40, 50];
+
+    for element in arr {
+        println!("current element = {element}")
+    }
+
+    println!("For loop done!")
+}
+
+fn _demo_for_reverse() {
+    for number in (1..6).rev() {
+        println!("current number = {number}")
+    }
+
+    println!("For loop reversed done!")
+}
+
+
+// ----------------------------------------------- //
+// ------------------ continue ------------------- //
+// ----------------------------------------------- //
+
+#[allow(unreachable_code)] // This silences the warning for the unreachable codes
+fn _demo_continue() {
+    for number in 1..=10 {
+        if (number % 2).eq(&0) {
+            println!("Even number = {number}");
+            continue
+        }
+        else {continue}
+
+        println!("The code reached here")
+    }
+}
+
+
+// ------------------------------------------- //
+// ------------------ pass ------------------- //
+// ------------------------------------------- //
+
+fn _demo_pass() {
+    for number in 1..=10 {
+        if (number % 2).eq(&0) {
+            println!("Even number = {number}");
+            continue
+        }
+        else {} // Just use empty scope to "pass" (like in python)
+
+        println!("The code reached here")
+    }
+}
+
+/////////////////////////////////
+///          main()           ///
+/////////////////////////////////
+
 fn main() {
     // _demo_loop_infinite();
-    demo_break();
-    demo_nested_loop_with_labels();
+    // _demo_break();
+    // _demo_nested_loop_with_labels();
+    // _demo_while();
+    // _demo_for_reverse();
+    // _demo_continue();
+    _demo_pass();
 }
